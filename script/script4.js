098d7f4fc2731e5b3b807cc6c124348dd7cecc84
misakihiyo <eva.shre@gmail.com>
2019-07-04 11:34:18 +0545

    edit page styling

---
+++ script/script4.js
@@ -0,0 +1,9 @@
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let logged = localStorage.getItem("currentlylogged");
+let icons = document.getElementsByClassName("icon-quill");
+
+
+icons.add
+
